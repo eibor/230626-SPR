3c08a1fd3a83620293a0c59a1ae4d97556fc246a
eibor <robbiemorgan5@gmail.com>
2023-06-27 20:37:31 +1000

    pretty much finished

--- SPR.py
+++ SPR.py
@@ -1,22 +1,21 @@
 # simple scissors paper rock game
 # computers choice is random
-# scissors (0) beats paper (1) beats rock (2) beats scissors (0)
+# logic is: scissors (0) beats paper (1) beats rock (2) beats scissors (0)
 
 import random
 
+# I thought writing this as a function would make it simpler 
+# it did not
 def setChoice(numChoice, strChoice):
-    retChoice = [numChoice]
+    retChoice = [numChoice,""]
     if (numChoice < 0 or numChoice > 2):
         # take strChoice 
         if (strChoice.lower() == "rock" or strChoice.lower() == "r"):
-            retChoice[0] = 2
-            retChoice.append("Rock")
+            retChoice = [2, "Rock"]
         elif(strChoice.lower() == "scissors" or strChoice.lower() == "s"):
-            retChoice[0] = 0
-            retChoice.append("Scissors")
+            retChoice = [0, "Scissors"]
         elif(strChoice.lower() == "paper" or strChoice.lower() == "p"):
-            retChoice[0] = 1
-            retChoice.append("Paper")
+            retChoice = [1, "Paper"]
         elif(strChoice.lower() == "end"):
             print("Thanks for playing!")
             exit()
@@ -24,19 +23,18 @@ def setChoice(numChoice, strChoice):
             print("Please give a valid input")
     # take numChoice
     elif (numChoice == 0):
-        retChoice[0] = 0
-        retChoice.append("Scissors")
+        retChoice = [0, "Scissors"]
     elif (numChoice == 1):
-        retChoice[0] = 1
-        retChoice.append("Paper")
+        retChoice = [1, "Paper"]
     else:
-        retChoice[0] = 2
-        retChoice.append("Rock")
+        retChoice = [2, "Rock"]
 
     return retChoice
 
 # begin main line
+# only way to end it is by typing end
 
+count = [0,0]
 print("Welcome to Scissors Paper Rock!!!!")
 while (True):
     humanChoice = ""
@@ -60,15 +58,18 @@ while (True):
     [computerChoiceNum, computerChoice] = setChoice(random.randint(0,2),"")
     print("I choose " + computerChoice)
 
-    # if subtract human from computer
-    # rock (2)
     minus = computerChoiceNum - humanChoiceNum
 
     if (minus == 0):
         print("It's a draw!")
     elif (minus == 1 or minus == -2):
         print("You won")
+        count[0] += 1
     elif (minus == -1 or minus == 2):
         print ("I won")
+        count[1] += 1
+    
+    print("Score is computer: " + str(count[1]) + " human: " + str(count[0]))
+    print()
         
     # print(minus)
