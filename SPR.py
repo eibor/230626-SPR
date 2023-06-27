de9c485f139b90f372680650509688ae627b495b
eibor <robbiemorgan5@gmail.com>
2023-06-27 20:24:20 +1000

    .

--- SPR.py
+++ SPR.py
@@ -4,6 +4,39 @@
 
 import random
 
+def setChoice(numChoice, strChoice):
+    retChoice = [numChoice]
+    if (numChoice < 0 or numChoice > 2):
+        # take strChoice 
+        if (strChoice.lower() == "rock" or strChoice.lower() == "r"):
+            retChoice[0] = 2
+            retChoice.append("Rock")
+        elif(strChoice.lower() == "scissors" or strChoice.lower() == "s"):
+            retChoice[0] = 0
+            retChoice.append("Scissors")
+        elif(strChoice.lower() == "paper" or strChoice.lower() == "p"):
+            retChoice[0] = 1
+            retChoice.append("Paper")
+        elif(strChoice.lower() == "end"):
+            print("Thanks for playing!")
+            exit()
+        else:
+            print("Please give a valid input")
+    # take numChoice
+    elif (numChoice == 0):
+        retChoice[0] = 0
+        retChoice.append("Scissors")
+    elif (numChoice == 1):
+        retChoice[0] = 1
+        retChoice.append("Paper")
+    else:
+        retChoice[0] = 2
+        retChoice.append("Rock")
+
+    return retChoice
+
+# begin main line
+
 print("Welcome to Scissors Paper Rock!!!!")
 while (True):
     humanChoice = ""
@@ -11,31 +44,20 @@ while (True):
     while (humanChoiceNum < 0):
         humanChoice = input("Please choose your input, type \"end\" to end the game: ")
 
-        if (humanChoice.lower() == "rock" or humanChoice.lower() == "r"):
-            humanChoiceNum = 2
-            humanChoice = "Rock"
-        elif(humanChoice.lower() == "scissors" or humanChoice.lower() == "s"):
-            humanChoiceNum = 0
-            humanChoice = "Scissors"
-        elif(humanChoice.lower() == "paper" or humanChoice.lower() == "p"):
-            humanChoiceNum = 1
-            humanChoice = "Paper"
-        elif(humanChoice.lower() == "end"):
-            exit()
-        else:
-            print("Please give a valid input")
+        [humanChoiceNum, humanChoice] = setChoice(-1, humanChoice)
             
 
     print("Your choice was: " + humanChoice)
 
-    computerChoiceNum = random.randint(0,2)
+    # computerChoiceNum = random.randint(0,2)
     # computerChoiceNum = 2
-    if (computerChoiceNum == 0):
-        computerChoice = "Scissors"
-    elif (computerChoiceNum == 1):
-        computerChoice = "Paper"
-    elif (computerChoiceNum == 2):
-        computerChoice = "Rock"
+    # if (computerChoiceNum == 0):
+    #     computerChoice = "Scissors"
+    # elif (computerChoiceNum == 1):
+    #     computerChoice = "Paper"
+    # elif (computerChoiceNum == 2):
+    #     computerChoice = "Rock"
+    [computerChoiceNum, computerChoice] = setChoice(random.randint(0,2),"")
     print("I choose " + computerChoice)
 
     # if subtract human from computer
